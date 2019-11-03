917290309551c84c6971f0a75b3db5f423fb0b98
Ally Morson <ally.morson@gmail.com>
2019-11-03 18:25:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,4 +11,20 @@ class Backer
     @back_project << Project.new 
   end 
 end 
+
+
+class Backer
+  attr_reader :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    backed_projects << project
+    project.backers << self
+  end
+
+end
     
